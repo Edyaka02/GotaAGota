2072e086d090304d36017ba43fed3d2309d418dd
Edgar Manuel Carrillo Muruato <132968940+Edyaka02@users.noreply.github.com>
2025-10-31 15:51:27 -0700

    Add files via upload

---
+++ GotaAGota/GotaAGota/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  GotaAGota
+//
+//  Created by Guest User on 31/10/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ GotaAGota/GotaAGota/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  GotaAGota
+//
+//  Created by Guest User on 31/10/25.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
